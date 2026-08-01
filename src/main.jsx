aee2db6f9d5aae16c17a49cee43336831e5d89e6
dragoumera <dragoumera@gmail.com>
2026-08-01 17:32:14 +0000

    Fix duplicate ReactDOM render calls in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import SLKManagerPage from './SLKManagerPage.jsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import SLKManagerPage from './SLKManagerPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/manager" element={<SLKManagerPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
