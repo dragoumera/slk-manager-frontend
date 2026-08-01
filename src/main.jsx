43daf160bee2f768018b568d5ccfb936b244f503
dragoumera <dragoumera@gmail.com>
2026-08-01 17:19:55 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import SLKManagerPage from './SLKManagerPage.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/manager" element={<SLKManagerPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
