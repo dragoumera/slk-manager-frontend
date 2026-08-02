19cf63b173f508186f3995e6de33438905530ae0
dragoumera <dragoumera@gmail.com>
2026-08-02 22:10:20 +0000

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import SLKManagerPrototype from "./SLKManager.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SLKManagerPrototype />
+  </React.StrictMode>
+);
